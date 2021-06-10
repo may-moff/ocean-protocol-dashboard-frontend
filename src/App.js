50f0078b4938771d8ce5658231baaa3ece19181a
MarekHalaczkiewicz <emhala8@gmail.com>
2021-06-10 20:05:38 +0200

    added FontSize and FontWeight to css variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="bg-bgradient text-center font-size-title">
+    <div className="bg-bpink text-center text-fslarge text-fwbase">
       <h1>Hello Dashboard Team!</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontSize: {
+        fsroot: "15px",
+        fsbase: "1rem",
+        fslarge: "1.2rem",
+        fssmall: "0.85rem",
+        fsmini: "0.65rem",
+        fstext: "1rem",
+        fslabel: "1rem",
+        fstitle: "1.4rem",
+        fsh1: "3rem",
+        fsh2: "2.5rem",
+        fsh3: "1.7rem",
+        fsh4: "1.3rem",
+        fsh5: "1.1rem",
+      },
+      FontWeight: {
+        fwbase: "500",
+        fwbold: "600",
+      },
       colors: {
         bwhite: "#ffffff",
         bblack: "#141414",
@@ -15,7 +34,7 @@ module.exports = {
         bgreylight: "#8b98a9",
         bgreylighter: "#e2e2e2",
         bgreydimmed: "#f7f7f7",
-        bgradient: "linear-gradient(to right bottom, #7b1173, #ff4092)",
+        bgradient: "linear-gradient(to right bottom #7b1173, #ff4092)",
         balertgreen: "#5fb359",
         balertred: "#d80606",
         balertorange: "#b35f36",
@@ -28,4 +47,23 @@ module.exports = {
   },
   plugins: [],
 };
-bgradient: "linear-gradient(to right bottom, #7b1173, #ff4092)",
+
+// https://tailwindcss.com/docs/font-size
+//   module.exports = {
+//     theme: {
+//       fontSize: {
+//        'xs': '.75rem',
+//        'sm': '.875rem',
+//        'tiny': '.875rem',
+//         'base': '1rem',
+//         'lg': '1.125rem',
+//         'xl': '1.25rem',
+//         '2xl': '1.5rem',
+//        '3xl': '1.875rem',
+//        '4xl': '2.25rem',
+//         '5xl': '3rem',
+//         '6xl': '4rem',
+//        '7xl': '5rem',
+//       }
+//     }
+//   }
