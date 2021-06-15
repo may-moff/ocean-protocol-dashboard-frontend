0247ced0f6dd8f886669a4b6d8cba6a36c57c471
Luca <luca.milanese90@gmail.com>
2021-06-15 11:01:57 +0200

    added props to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 const Card = ({ additionalClasses, children }) => {
   return (
     <div
-      className={`bg-background-content rounded-md shadow-lg ${
+      className={`bg-background-content rounded-md shadow-lg transform hover:-translate-y-1 w-1/5 h-52 p-12${
         additionalClasses || ''
       }`}
     >
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,11 @@ import Card from '../components/Card';
 
 function Home() {
   return (
-    <Card additionalClasses="text-center text-fslarge text-fwbase">
-      <h1>Hello Dashboard Team!</h1>
-    </Card>
+    <div className="flex-row ">
+      <Card additionalClasses="text-center text-fslarge text-fwbase">
+        <h1>Hello Dashboard Team!</h1>
+      </Card>
+    </div>
   );
 }
 
