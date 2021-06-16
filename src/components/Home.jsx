bc41b5a71f20fa4b1ad17b4ad3c4c877f6eb21f4
Maya Imhoff <mayelyimhoff@gmail.com>
2021-06-16 14:01:34 +0200

    started implementing web3 login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import Login from "./Login";
 
 function Home() {
   return (
-    <div className="bg-bpink text-center text-fslarge text-fwbase">
-      <h1>Hello Dashboard Team!</h1>
-    </div>
+    <>
+      <div className="bg-bpink text-center text-fslarge text-fwbase">
+        <h1>Hello Dashboard Team!</h1>
+      </div>
+      <Login />
+    </>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Web3 from "web3";
+import MetaMaskLoginButton from "react-metamask-login-button";
+
+// let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+let web3 = new Web3(Web3.givenProvider || undefined);
+// web3.eth.getAccounts().then(console.log);
+// web3.eth.getCoinbase().then(console.log);
+console.log(web3);
+
+function Login() {
+  const handleClick = async () => {
+    // Check if MetaMask is installed
+    if (!window.ethereum) {
+      window.alert("Please install MetaMask first.");
+    }
+
+    if (!web3) {
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+
+        // We don't know window.web3 version, so we use our own instance of Web3
+        // with the injected provider given by MetaMask
+        web3 = new Web3(window.ethereum);
+      } catch (error) {
+        window.alert("You need to allow MetaMask.");
+      }
+    }
+
+    const coinbase = await web3.eth.getCoinbase();
+    console.log(coinbase);
+    console.log("hi");
+    if (!coinbase) {
+      window.alert("Please activate MetaMask first.");
+    }
+  };
+
+  //   const publicAddress = coinbase.toLowerCase();
+  //   setLoading(true);
+
+  return (
+    <div>
+      {/* <MetaMaskLoginButton /> */}
+      <button onClick={handleClick}>Login with MetaMask</button>
+    </div>
+  );
+}
+
+export default Login;
