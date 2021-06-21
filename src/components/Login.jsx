938a761430b9d2be447cefeda78d81c10df285dc
Luca <luca.milanese90@gmail.com>
2021-06-21 15:12:03 +0200

    fixed response

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Web3 from "web3";
-import axios from "axios";
-import ButtonPrimary from "./ButtonPrimary";
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import axios from 'axios';
+import ButtonPrimary from './ButtonPrimary';
 
 let web3 = new Web3(Web3.givenProvider || undefined);
 
@@ -24,29 +24,31 @@ function Login({ onLoggedIn }) {
       const signature = await web3.eth.personal.sign(
         `I am signing my one-time nonce: ${nonce}`,
         publicAddress,
-        ""
+        ''
       );
 
       return { publicAddress, signature };
     } catch (err) {
-      throw new Error("You need to sign the message to be able to log in.");
+      throw new Error('You need to sign the message to be able to log in.');
     }
   };
 
-  const handleSignup = (publicAddress) =>
+  const handleSignup = (publicAddress) => {
+    console.log(JSON.stringify({ publicAddress }));
     axios
       .post(`${process.env.REACT_APP_BACKEND_URL}/users`, {
         body: JSON.stringify({ publicAddress }),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       })
       .then((response) => response.json());
+  };
 
   const handleClickMeta = async () => {
     // Check if MetaMask is installed
     if (!window.ethereum) {
-      return window.alert("Please install MetaMask first.");
+      return window.alert('Please install MetaMask first.');
     }
 
     if (!web3) {
@@ -58,31 +60,35 @@ function Login({ onLoggedIn }) {
         // with the injected provider given by MetaMask
         web3 = new Web3(window.ethereum);
       } catch (error) {
-        return window.alert("You need to allow MetaMask.");
+        return window.alert('You need to allow MetaMask.');
       }
     }
 
     const coinbase = await web3.eth.getCoinbase();
-    console.log("my public address is", coinbase);
-    console.log("hi");
+    console.log('my public address is', coinbase);
+    console.log('hi');
     if (!coinbase) {
-      return window.alert("Please activate MetaMask first.");
+      return window.alert('Please activate MetaMask first.');
     }
 
     // const publicAddress = coinbase.toLowerCase();
     const publicAddress = coinbase.toLowerCase();
     setLoading(true);
-    console.log("before call", publicAddress);
+    console.log('before call', publicAddress);
     // Look if user with current publicAddress is already present on backend
 
     axios
       .get(
         `${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=${publicAddress}`
       )
-      .then((response) => response.data.publicAddress)
+      // .then((response) => console.log(response))
       // .then((data) => data.publicAddress)
+      .then((response) => {
+        const users = response.data;
+        return users.length ? users[0] : handleSignup(publicAddress);
+      })
 
-      .then((users) => (users.length ? users[0] : handleSignup(publicAddress)))
+      // (users.length ? users[0] : handleSignup(publicAddress)))
       // Popup MetaMask confirmation modal to sign message
       .then(handleSignMessage)
       // Send signature to backend on the /auth route
@@ -99,8 +105,8 @@ function Login({ onLoggedIn }) {
   return (
     <div>
       <button onClick={handleClickMeta}>
-        {loading ? "Loading..." : "Login with MetaMask"}
-      </button>{" "}
+        {loading ? 'Loading...' : 'Login with MetaMask'}
+      </button>{' '}
     </div>
   );
 }
