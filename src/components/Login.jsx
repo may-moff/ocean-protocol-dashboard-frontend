05d44d0a5351cb7e190742e13b3f479363320bf1
Maya Imhoff <mayelyimhoff@gmail.com>
2021-06-21 12:01:24 +0200

    added publicaddress get and post requests

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import axios from "axios";
+import ButtonPrimary from "./ButtonPrimary";
 
-// let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 let web3 = new Web3(Web3.givenProvider || undefined);
-// web3.eth.getAccounts().then(console.log);
-// web3.eth.getCoinbase().then(console.log);
+
 console.log(web3);
 
-function Login() {
-  //   const handleSignup = ({ publicAddress }) =>
-  //     fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
-  //       body: JSON.stringify({ publicAddress }),
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       method: "POST",
-  //     }).then((response) => response.json());
+function Login({ onLoggedIn }) {
+  const [loading, setLoading] = useState(false);
+
+  // const handleAuthenticate = ({ publicAddress, signature }) =>
+  //   fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
+  //     body: JSON.stringify({ publicAddress, signature }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "POST",
+  //   }).then((response) => response.json());
+
+  const handleSignMessage = async ({ publicAddress, nonce }) => {
+    try {
+      const signature = await web3.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
+        publicAddress,
+        ""
+      );
+
+      return { publicAddress, signature };
+    } catch (err) {
+      throw new Error("You need to sign the message to be able to log in.");
+    }
+  };
+
+  const handleSignup = (publicAddress) =>
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/users`, {
+        body: JSON.stringify({ publicAddress }),
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
+      })
+      .then((response) => response.json());
 
   const handleClickMeta = async () => {
     // Check if MetaMask is installed
     if (!window.ethereum) {
-      window.alert("Please install MetaMask first.");
+      return window.alert("Please install MetaMask first.");
     }
 
     if (!web3) {
@@ -33,7 +58,7 @@ function Login() {
         // with the injected provider given by MetaMask
         web3 = new Web3(window.ethereum);
       } catch (error) {
-        window.alert("You need to allow MetaMask.");
+        return window.alert("You need to allow MetaMask.");
       }
     }
 
@@ -41,27 +66,38 @@ function Login() {
     console.log("my public address is", coinbase);
     console.log("hi");
     if (!coinbase) {
-      window.alert("Please activate MetaMask first.");
+      return window.alert("Please activate MetaMask first.");
     }
 
+    // const publicAddress = coinbase.toLowerCase();
     const publicAddress = coinbase.toLowerCase();
+    setLoading(true);
+    console.log(publicAddress);
     // Look if user with current publicAddress is already present on backend
-
     axios
       .get(
-        `${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=${publicAddress}`
+        `${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=0x0e6a88973321a43899d9c97412995e491ef4aac2`
       )
-      .then((response) => response.data)
-      .then((data) => {
-        console.log(data);
+      .then((response) => console.log(response.data))
+      .then((users) => (users.length ? users[0] : handleSignup(publicAddress)))
+      // Popup MetaMask confirmation modal to sign message
+      .then(handleSignMessage)
+      // Send signature to backend on the /auth route
+      // .then(handleAuthenticate)
+      // Pass accessToken back to parent component (to save it in localStorage)
+      .then(onLoggedIn)
+      .catch((err) => {
+        window.alert(err);
+        console.log(err);
+        setLoading(false);
       });
-
-    //   !!!!!!!!!!!!!!!!!!!stop!! function ends after this line!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   };
 
   return (
     <div>
-      <button onClick={handleClickMeta}>Login with MetaMask</button>
+      <button className="Login-button Login-mm" onClick={handleClickMeta}>
+        {loading ? "Loading..." : "Login with MetaMask"}
+      </button>{" "}
     </div>
   );
 }
