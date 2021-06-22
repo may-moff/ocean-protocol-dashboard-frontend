4ac9c892bbc150c4da5d187baedd165dfe20d2b7
Marek <emhala8@gmail.com>
2021-06-22 14:56:37 +0200

    started with Hero page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,22 @@ import wave from "./wave/Waves.gif"
 function Home() {
   return (
     <>
-    <div className=" m-auto">
-      <h1 className="text-center text-bblack text-fsh1 font-fwbase p-6">
-        Welcome to the dashdoard
+    <div className="">
+      <h1 className="text-center text-bblack text-fsh1 font-fwbase mt-6">
+        Ocean Dashboard
       </h1>
-      <img src={wave} alt="wave" />
+      <img className="w-full" src={wave} alt="wave" />
     </div>
       <div>
         <img
-          className="inline mb-3"
+          className="inline mb-3 max-w-12 max-h-12"
           src={jellyfish} 
           alt="logo"
         />
       </div>
       <div>
    
-      <p className="font-fsbase">Lorem Ipsum</p>
+      <p className="font-fsbase"></p>
       </div>
       </>
   )};
