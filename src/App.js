91cd425b22529386aff1bd344ae4ebd20a414a95
Maya Imhoff <mayelyimhoff@gmail.com>
2021-06-22 13:42:51 +0200

    too manu rerenders

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import { Login } from "./components/Login";
-// import { Profile } from "./components/Profile";
+import { Profile } from "./components/Profile";
 
 // ???????????????????
 const LS_KEY = "login-with-metamask:auth";
@@ -25,14 +25,15 @@ export const App = () => {
   // }, []);
 
   const handleLoggedIn = (auth) => {
+    console.log(auth);
     localStorage.setItem(LS_KEY, JSON.stringify(auth));
     setState({ auth });
   };
 
-  // const handleLoggedOut = () => {
-  //   localStorage.removeItem(LS_KEY);
-  //   setState({ auth: undefined });
-  // };
+  const handleLoggedOut = () => {
+    localStorage.removeItem(LS_KEY);
+    setState({ auth: undefined });
+  };
 
   const { auth } = state;
 
@@ -45,19 +46,18 @@ export const App = () => {
           <Login onLoggedIn={handleLoggedIn} />
         )}
       </div> */}
-
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/dashboard">
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Login with MetaMask Demo</h1>
+        </header>
+        <div className="App-intro">
+          {auth ? (
+            <Profile auth={auth} onLoggedOut={handleLoggedOut} />
+          ) : (
             <Login onLoggedIn={handleLoggedIn} />
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
+          )}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,29 +3,6 @@ import Blockies from "react-blockies";
 import jwt_decode from "jwt-decode";
 // import jwt_decode from "jwt-decode";
 
-// var token = "eyJ0eXAiO.../// jwt token";
-// var decoded = jwt_decode(token);
-// interface Props {
-//   auth: Auth;
-//   onLoggedOut: () => void;
-// }
-
-// interface State {
-//   loading: boolean;
-//   user?: {
-//     id: number,
-//     username: string,
-//   };
-//   username: string;
-// }
-
-// interface jwt_decoded {
-//   payload: {
-//     id: string,
-//     publicAddress: string,
-//   };
-// }
-
 export const Profile = ({ auth, onLoggedOut }) => {
   const [state, setState] = useState({
     loading: false,
@@ -51,36 +28,12 @@ export const Profile = ({ auth, onLoggedOut }) => {
 
   setState({ ...state, loading: true });
 
-  if (!user) {
-    window.alert(
-      "The user id has not been fetched yet. Please try again in 5 seconds."
-    );
-    return;
-  }
-
-  fetch(`${process.env.REACT_APP_BACKEND_URL}/users/${user.id}`, {
-    // body: JSON.stringify({ username }),
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
-    },
-    method: "PATCH",
-  })
-    .then((response) => response.json())
-    .then((user) => setState({ ...state, loading: false, user }))
-    .catch((err) => {
-      window.alert(err);
-      setState({ ...state, loading: false });
-    });
-
   const { accessToken } = auth;
 
   const {
     payload: { publicAddress },
   } = jwt_decode(accessToken);
   //   jwt_decode<jwt_decoded>(accessToken);
-
-  const { loading, user } = state;
 
   return (
     <div className="Profile">
