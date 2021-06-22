d3de12bec7426ba6f4de2f3d5a0469497bee8067
Maya Imhoff <mayelyimhoff@gmail.com>
2021-06-22 06:33:07 +0200

    added auth axios post

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import axios from 'axios';
-import ButtonPrimary from './ButtonPrimary';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import axios from "axios";
+import ButtonPrimary from "./ButtonPrimary";
 
 let web3 = new Web3(Web3.givenProvider || undefined);
 
@@ -19,18 +19,34 @@ function Login({ onLoggedIn }) {
   //     method: "POST",
   //   }).then((response) => response.json());
 
+  const handleAuthenticate = async (publicAddress, signature) => {
+    console.log(publicAddress, "YOOOOOOOOOOOO");
+    try {
+      const userAuth = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/auth`,
+        {
+          publicAddress,
+          signature,
+        }
+      );
+      return userAuth.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const handleSignMessage = async ({ publicAddress, nonce }) => {
     console.log(publicAddress, nonce);
     try {
       const signature = await web3.eth.personal.sign(
         `I am signing my one-time nonce: ${nonce}`,
         publicAddress,
-        ''
+        ""
       );
 
       return { publicAddress, signature };
     } catch (err) {
-      throw new Error('You need to sign the message to be able to log in.');
+      throw new Error("You need to sign the message to be able to log in.");
     }
   };
 
@@ -46,24 +62,12 @@ function Login({ onLoggedIn }) {
     } catch (err) {
       console.log(err);
     }
-    //   .then((response) => {
-    //     console.log(response);
-    //     return response.data;
-    //   });
-    // axios
-    //   .post(`${process.env.REACT_APP_BACKEND_URL}/users`, {
-    //     publicAddress,
-    //   })
-    //   .then((response) => {
-    //     console.log(response);
-    //     return response.data;
-    //   });
   };
 
   const handleClickMeta = async () => {
     // Check if MetaMask is installed
     if (!window.ethereum) {
-      return window.alert('Please install MetaMask first.');
+      return window.alert("Please install MetaMask first.");
     }
 
     if (!web3) {
@@ -75,21 +79,21 @@ function Login({ onLoggedIn }) {
         // with the injected provider given by MetaMask
         web3 = new Web3(window.ethereum);
       } catch (error) {
-        return window.alert('You need to allow MetaMask.');
+        return window.alert("You need to allow MetaMask.");
       }
     }
 
     const coinbase = await web3.eth.getCoinbase();
-    console.log('my public address is', coinbase);
-    console.log('hi');
+    console.log("my public address is", coinbase);
+    console.log("hi");
     if (!coinbase) {
-      return window.alert('Please activate MetaMask first.');
+      return window.alert("Please activate MetaMask first.");
     }
 
     // const publicAddress = coinbase.toLowerCase();
     const publicAddress = coinbase.toLowerCase();
     setLoading(true);
-    console.log('before call', publicAddress);
+    console.log("before call", publicAddress);
     // Look if user with current publicAddress is already present on backend
 
     axios
@@ -106,7 +110,7 @@ function Login({ onLoggedIn }) {
       // (users.length ? users[0] : handleSignup(publicAddress)))
       // Popup MetaMask confirmation modal to sign message
       // Send signature to backend on the /auth route
-      // .then(handleAuthenticate)
+      .then(handleAuthenticate)
       // Pass accessToken back to parent component (to save it in localStorage)
       .then(onLoggedIn)
       .catch((err) => {
@@ -119,8 +123,8 @@ function Login({ onLoggedIn }) {
   return (
     <div>
       <button onClick={handleClickMeta}>
-        {loading ? 'Loading...' : 'Login with MetaMask'}
-      </button>{' '}
+        {loading ? "Loading..." : "Login with MetaMask"}
+      </button>{" "}
     </div>
   );
 }
