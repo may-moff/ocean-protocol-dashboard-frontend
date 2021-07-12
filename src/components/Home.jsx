68481d79b78da67e0a3a7d01d630d5428e8c5d61
Luca <luca.milanese90@gmail.com>
2021-07-12 13:49:32 +0200

    added wave component from d3 library

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import jellyfish from "./../Assets/jellyfish-full.svg"
-import wave from "./wave/Waves.gif"
+import jellyfish from './../Assets/jellyfish-full.svg';
+import wave from './wave/Waves.gif';
+import Waves from './Waves';
 
 function Home() {
   return (
     <>
-    <div className="">
-      <h1 className="text-center text-bblack text-fsh1 font-fwbase mt-6">
-        Ocean Dashboard
-      </h1>
-      <img className="w-full" src={wave} alt="wave" />
-    </div>
-      <div>
-   
-      <p className="font-fsbase"></p>
+      <div className="">
+        <h1 className="text-center text-bblack text-fsh1 font-fwbase mt-6">
+          Ocean Dashboard
+        </h1>
+        {/* <img className="w-full" src={wave} alt="wave" /> */}
       </div>
-      </>
-  )};
+      <Waves />
+      <div>
+        <p className="font-fsbase"></p>
+      </div>
+    </>
+  );
+}
 
 export default Home;
