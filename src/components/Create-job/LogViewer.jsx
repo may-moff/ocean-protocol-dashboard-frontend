9641d76e1f3527f3b10e110d90dcca54ba841f39
Marek <emhala8@gmail.com>
2021-07-15 21:23:02 +0200

    Tailwind NewJob - not finished

--- src/components/Create-job/LogViewer.jsx
+++ src/components/Create-job/LogViewer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LogViewer = ({ file }) => {
   return (
-    <iframe src={file} title="file preview" className="w-full" height="600px" />
+    <iframe src={file} title="file preview" className="w-full max-h-screen" />
   );
 };
 
--- src/components/FormParse.jsx
+++ src/components/FormParse.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 function FormParse({ content, setContent }) {
   useEffect(() => {
@@ -23,52 +23,50 @@ function FormParse({ content, setContent }) {
   };
 
   const handleAddClick = () => {
-    setContent([...content.parseKeys, { key: '', dataType: '', value: '' }]);
+    setContent([...content.parseKeys, { key: "", dataType: "", value: "" }]);
     setContent({
       ...content,
-      parseKeys: [...content.parseKeys, { key: '', dataType: '', value: '' }],
+      parseKeys: [...content.parseKeys, { key: "", dataType: "", value: "" }],
     });
   };
 
   return (
-    <div className="max-h-screen">
+    <div className="max-h-155 overflow-y-scroll ">
+      <div className="flex border rounded m-1 font-bold p-1">
+        <div className="border-1 p-1 w-2/12">Key:</div>
+        <div className="border-1 p-1 w-2/12">Type:</div>
+        <div className="border-1 p-2 w-7/12">Value:</div>
+      </div>
       {content &&
         content.parseKeys.map((x, i) => {
           return (
-            <div key={i} className="m-1">
+            <div key={i} className="m-1 text-base">
               <div className="hover:bg-bgreylighter">
                 <div className="flex border rounded m-1">
-                  <div className="flex place-items-center font-bold p-1">
-                    Key:
-                  </div>
                   <input
-                    className="border-1 p-1 w-1/12"
+                    className="border-2 p-1 w-2/12"
                     name="key"
                     placeholder="Key"
-                    value={x.key || ''}
+                    value={x.key || ""}
                     onChange={(e) => handleInputChange(e, i)}
                   />
-                  <div className="flex place-items-center font-bold p-1">
-                    Type:
-                  </div>
+
                   <input
-                    className="border-1 p-1 w-1/12"
+                    className="border-2 p-1 w-2/12"
                     name="type"
                     placeholder="Type"
-                    value={x.dataType || ''}
+                    value={x.dataType || ""}
                     onChange={(e) => handleInputChange(e, i)}
                   />
-                  <div className="flex place-items-center font-bold p-1">
-                    Value:
-                  </div>
+
                   <input
-                    className="border-1 p-2 w-3/6"
+                    className="border-2 p-2 w-7/12"
                     name="value"
                     placeholder="Value"
                     readOnly
-                    value={x.value || ''}
+                    value={x.value || ""}
                   />
-                  <div className="flex object-center w-1/12 ">
+                  <div className="flex object-center w-20">
                     <div>
                       {content.parseKeys.length !== 1 && (
                         <button
@@ -80,18 +78,18 @@ function FormParse({ content, setContent }) {
                         </button>
                       )}
                     </div>
-                    <div>
-                      {content.parseKeys.length - 1 === i && (
-                        <button
-                          className="bg-bpink text-white py-2 px-6 font-semibold rounded transform hover:-translate-y-0.5 duration-300"
-                          onClick={handleAddClick}
-                        >
-                          Add
-                        </button>
-                      )}
-                    </div>
                   </div>
                 </div>
+              </div>
+              <div>
+                {content.parseKeys.length - 1 === i && (
+                  <button
+                    className="bg-bpink text-white py-2 px-6 font-semibold rounded transform hover:-translate-y-0.5 duration-300"
+                    onClick={handleAddClick}
+                  >
+                    Add
+                  </button>
+                )}
               </div>
             </div>
           );
