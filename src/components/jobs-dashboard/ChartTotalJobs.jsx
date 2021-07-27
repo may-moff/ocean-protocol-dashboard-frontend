c93ef2361e417a072ea73c6e7986a454a973ea55
Luca <luca.milanese90@gmail.com>
2021-07-27 17:17:44 +0200

    fixed merging issues

--- src/components/jobs-dashboard/ChartTotalJobs.jsx
+++ src/components/jobs-dashboard/ChartTotalJobs.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { getMonthFromDate } from '../helpers/textManipulation'
+import { getMonthFromDate } from '../../helpers/textManipulation'
 import {
   BarChart,
   Bar,
