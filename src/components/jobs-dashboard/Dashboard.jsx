f873b3ff240adb68ec28d297f21bb22a02aa286a
maya <mayelyimhoff@gmail.com>
2021-07-28 17:42:30 +0200

    algoname and jobname search working

--- src/components/jobs-dashboard/Dashboard.jsx
+++ src/components/jobs-dashboard/Dashboard.jsx
@@ -15,12 +15,10 @@ const Dashboard = ({ jobList, setJobList }) => {
   //     (job.jobName && job.jobName.toLocaleLowerCase().includes(search)) ||
   //     job.algorithmId.includes(search)
   // )
-  // const filteredJobs = jobList.filter(
-  //   (job) => job.jobName && job.jobName.toLocaleLowerCase().includes(search)
-  // )
   const filteredJobs = jobList.filter(
     (job) =>
-      (job._id && job._id.toLocaleLowerCase().includes(search)) ||
+      (job.algorithmId.algoName &&
+        job.algorithmId.algoName.toLocaleLowerCase().includes(search)) ||
       (job.jobName && job.jobName.toLocaleLowerCase().includes(search))
   )
 
@@ -40,15 +38,14 @@ const Dashboard = ({ jobList, setJobList }) => {
           filteredJobs.map((job, i) => {
             return (
               <div key={i} className="cards-container">
-                <h2>Job Name:</h2>
-                <h2 className="font-bold pl-1">{job.jobName}</h2>
-                <h2>{job.algorithmId.algoName}</h2>
+                <h2>Job Name: {job.jobName}</h2>
 
-                <h2>Algorithm Name:</h2>
-                <h2 className="font-bold pl-1">{job._id}</h2>
+                <h2>AlgoName : {job.algorithmId.algoName}</h2>
 
-                <h2>Date:</h2>
-                <h2 className="font-bold pl-1">{job.date.slice(0, 10)}</h2>
+                <h2>ID: {job._id}</h2>
+                <h2 className="font-bold pl-1"></h2>
+
+                <h2>Date:{job.date.slice(0, 10)}</h2>
               </div>
             )
           })}
