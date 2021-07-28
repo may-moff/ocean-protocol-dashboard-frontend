fba74be97d1bb28d040f8b0f2b34e61f95066284
Luca <luca.milanese90@gmail.com>
2021-07-28 10:37:48 +0200

    fixed main barchart count

--- src/components/jobs-dashboard/ChartTotalJobs.jsx
+++ src/components/jobs-dashboard/ChartTotalJobs.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React from 'react'
 import { getMonthFromDate } from '../../helpers/textManipulation'
 import {
   BarChart,
@@ -63,43 +63,41 @@ const data = [
 ]
 
 const ChartTotalJobs = ({ jobList }) => {
-  const [jobCount, setJobCount] = useState(data)
-
-  const newJobCount = useCallback(() => {
-    const output = [...data]
-    jobList.forEach((job) => {
+  const newJobCount = (jobs) => {
+    const output = data.map((e) => ({ ...e }))
+    jobs.forEach((job) => {
       const currentMonth = getMonthFromDate(job.date)
       const index = output.findIndex((x) => x.name === currentMonth)
       output[index].total++
     })
-    setJobCount(output)
-  }, [jobList])
-
-  useEffect(() => newJobCount(), [newJobCount])
+    return output
+  }
 
   return (
     <div className="tablet:h-4/5">
-      <ResponsiveContainer width="100%" aspect={8}>
-        <BarChart
-          width={600}
-          height={200}
-          data={jobCount}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          {/* <Legend /> */}
-          {/* <Bar dataKey="Tagged" stackId="a" fill="#8b98a9" /> */}
-          <Bar dataKey="total" stackId="a" fill="#7b1173" />
-        </BarChart>
-      </ResponsiveContainer>
+      {jobList.length > 0 && (
+        <ResponsiveContainer width="100%" aspect={8}>
+          <BarChart
+            width={600}
+            height={200}
+            data={newJobCount(jobList)}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            {/* <Legend /> */}
+            {/* <Bar dataKey="Tagged" stackId="a" fill="#8b98a9" /> */}
+            <Bar dataKey="total" stackId="a" fill="#7b1173" />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
     </div>
   )
 }
